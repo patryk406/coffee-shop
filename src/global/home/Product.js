4fe5fde182dc0c4c036bc7e7f1067dee0128e73e
patryk406 <Merph406@gmail.com>
2020-11-07 20:02:16 +0100

    adding key parameter

--- src/global/home/Product.js
+++ src/global/home/Product.js
@@ -34,7 +34,10 @@ export default function Product() {
               <div className="row">
                 {data.products.edges.map(({ node: products }) => {
                   return (
-                    <div className="col-10 col-sm-8 col-md-6 col-lg 4 mx-auto my-3">
+                    <div
+                      key={products.id}
+                      className="col-10 col-sm-8 col-md-6 col-lg 4 mx-auto my-3"
+                    >
                       <div className="card" style={{ minHeight: "100%" }}>
                         <Img
                           fluid={products.image.fluid}
